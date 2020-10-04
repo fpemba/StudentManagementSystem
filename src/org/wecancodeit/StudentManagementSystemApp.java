a5fc3b03dfd334031821801856a9f4cae47f7455
fpemba <fatuma.pemba@yahoo.com>
2020-10-04 10:59:01 -0400

    main method

--- src/org/wecancodeit/StudentManagementSystemApp.java
+++ src/org/wecancodeit/StudentManagementSystemApp.java
@@ -1,8 +1,26 @@
 package org.wecancodeit;
 
+import java.util.Scanner;
+
 public class StudentManagementSystemApp {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
+        for (int i = 0; i < numOfStudents; i++) {
+            students[i] = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+            System.out.println("***********************************");
+        }
+
+        for (int j = 0; j < numOfStudents; j++) {
+            System.out.println(students[j].showInfo());
+            System.out.println("************************************");
+        }
+
     }
 }
